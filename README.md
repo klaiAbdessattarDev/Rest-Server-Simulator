ebba7633843e60dbbc6da75e6819c4eff779b935
Abdessattar Klai <klaistoura@gmail.com>
2024-03-10 21:04:08 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,24 @@
+
+## Description
+ REST server Simulator
+- simulator which can send several requests for POST customers and GET
+customers in parallel to the server.
+
+The simulated POST customers requests have following requirements:
+- Each request should contain at least 2 different customers
+- Age should be randomized between 10 and 90
+- ID should be increasing sequentially.
+- The first names and last names of the Appendix should be used in random combinations
+
+## Tech Stack
+
+**Server:** C#, ,Net 5
+
+
+## 🚀 About Me
+I'm a senior ,net software engineer, I have over seven years of experience in developing web solutions using .NET technologies, such as ASP.NET MVC, Web aPI, Razor, Entity Framework, SQL Server, JavaSCript, Jqury, TypeScript,Azure DevOps.
+
+I enjoy taking on challenging projects that require creativity, collaboration, and quality assurance. I follow clean code best practices, conduct code reviews, and write unit and integration tests using NUnit and Pact. I also have experience in front-end development with Razor and KnockoutJs, and in working with Postgresql and SQL Server databases. My goal is to deliver reliable, scalable, and user-friendly solutions that meet the needs of the clients and the users....
+
+- Linkedin : https://www.linkedin.com/in/abdessattar-klai-devweb/
+- Github : https://github.com/klaiAbdessattarDev/
---
+++ Rest Server Simulator/Program.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+
+namespace Rest_Server_Simulator
+{
+    // Simulator class to simulate sending requests to a REST server
+    public class Simulator
+    {
+        private static readonly HttpClient _httpClient = new();
+
+        private static readonly string _BaseUrl = "https://localhost:44316/Customers";
+
+        // Main method to run the simulator
+        public static async Task Main(string[] args)
+        {
+            // List of tasks to run concurrently
+            var tasks = new List<Task>
+        {
+            SendPostCustomersRequestAsync(),
+            SendGetCustomersRequestAsync()
+        };
+
+            // Wait for all tasks to complete
+            await Task.WhenAll(tasks);
+        }
+
+        // Method to send a POST request to add customers
+        private static async Task SendPostCustomersRequestAsync()
+        {
+            var customers = GenerateCustomers();
+
+            // Send a POST request for each customer
+            foreach (var customer in customers)
+            {
+                List<Customer> customerRequest = new() { customer };
+                var response = await _httpClient.PostAsJsonAsync(_BaseUrl, customerRequest);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+        // Method to send a GET request to retrieve customers
+        private static async Task SendGetCustomersRequestAsync()
+        {
+            // Send a GET request to retrieve customers
+            var response = await _httpClient.GetAsync(_BaseUrl);
+            response.EnsureSuccessStatusCode();
+            var customers = await response.Content.ReadFromJsonAsync<List<Customer>>();
+            Console.WriteLine("Customers:");
+
+            // Print the details of each customer
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"{customer.Id}: {customer.LastName}, {customer.FirstName}, {customer.Age}");
+            }
+        }
+
+        // Method to generate a list of customers
+        private static List<Customer> GenerateCustomers()
+        {
+            var customers = new List<Customer>();
+
+            var random = new Random();
+
+            // Array of customer data objects
+            var data = new[]
+            {
+            new { FirstName = "Leia", LastName = "Liberty" },
+            new { FirstName = "Sadie", LastName = "Ray" },
+            new { FirstName = "Jose", LastName = "Harrison" },
+            new { FirstName = "Sara", LastName = "Ronan" },
+            new { FirstName = "Frank", LastName = "Drew" },
+            new { FirstName = "Dewey", LastName = "Powell" },
+            new { FirstName = "Tomas", LastName = "Larsen" },
+            new { FirstName = "Joel", LastName = "Chan" },
+            new { FirstName = "Lukas", LastName = "Anderson" },
+            new { FirstName = "Carlos", LastName = "Lane" }
+        };
+
+            // Generate 10 customers
+            for (int i = 0; i < 10; i++)
+            {
+                // Select a random customer data object
+                var customerData = data[random.Next(0, data.Length)];
+
+                // Generate a random age
+                var age = random.Next(10, 90);
+
+                // Generate a unique ID using the hash code of a new Guid
+                var id = Guid.NewGuid().GetHashCode();
+
+                // Create a new customer object with the generated data
+                customers.Add(new Customer { FirstName = customerData.FirstName, LastName = customerData.LastName, Age = age, Id = id });
+            }
+
+            return customers;
+        }
+    }
+
+    public class Customer
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+    }
+}
